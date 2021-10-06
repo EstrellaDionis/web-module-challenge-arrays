52a1231fddd9e13fa8ea1e8540db60c820661c97
EstrellaDionis <EstrellaDionis@gmail.com>
2021-10-06 17:49:17 -0400

    completed all mvp objectives

--- index.js
+++ index.js
@@ -45,9 +45,10 @@ Use the copy function below to do the following:
   2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(array){
+  return [...array]
 }    
+console.log('task 1:', copy(originalFlavors));
 
 
 
@@ -63,10 +64,15 @@ Confirm that an array is exactly 31 flavors. Your function should accept:
 For Example: is31Flavors(originalFlavors) will return true if your code is working properly
 */
 
-
-function is31Flavors(/*your code here*/){
- /*your code here*/
+//1 parameter called array
+function is31Flavors(array){
+  if(array.length === 31){
+    return true;
+  }else{
+    return false;
+  }
 }
+console.log('task 2:', is31Flavors(originalFlavors));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -80,10 +86,14 @@ Use the addFlavor function below to do the following:
   For example: addFlavor(originalFlavors, "Rainbow Sherbert") should return the array ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"]
 */
 
-
-function addFlavor(/*your code here*/){
- /*your code here*/
+// 2 parameters - array, flavor
+function addFlavor(array, string){
+  array.unshift(string)
+  return array;
+ // add to the front of an array - unshift
+ // return the array
 }
+console.log('task 3:', addFlavor(originalFlavors, 'Rainbow Sherbert'));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -96,10 +106,14 @@ Use the removeLastFlavor function below to do the following:
 
   For example: running removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla"]
 */
-
-function removeLastFlavor(/*your code here*/){
- /*your code here*/
+// 1 parameter - array
+function removeLastFlavor(array){
+ array.pop();
+ return array;
+ // remove the item at the end of the array - remember .pop() does not need an argument
+ // return the array 
 }
+console.log('task 4:', removeLastFlavor(originalFlavors));
 
 
 
@@ -107,16 +121,19 @@ function removeLastFlavor(/*your code here*/){
 Write a function that returns a flavor at a given index in the array.
 
 Use the getFlavorByIndex function below to do the following:
-  1. Recieve an array
+  1. Receive an array
   2. Receive a number (the desired index)
   3. Return the flavor located at the received index position
 
   For example: running getFlavorByIndex(originalFlavors, 2) would return "Black Walnut", assuming Rainbow Sherbert has been added successfully
 */
 
-function getFlavorByIndex(/*your code here*/){
-  /*your code here*/
+// 2 param - array, index
+function getFlavorByIndex(array, number){
+  return array[number];
+  // return array[index]
 }
+console.log('task 5:', getFlavorByIndex(originalFlavors, 2));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -134,9 +151,16 @@ Use the removeFlavorByName function below to do the following:
   HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-  /*your code here*/
+// 2 params array, string
+function removeFlavorByName(array, flavor){
+    for(let i = 0; i < array.length; i++){
+      if(array[i] === flavor){
+        array.splice(i, 1);
+      }
+    }
+    return array;
 }
+console.log('task 6:', removeFlavorByName(originalFlavors, 'Rocky Road'));
 
 
 
@@ -160,9 +184,18 @@ Use the filterByWord function below to do the following:
   DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem. 
 */
 
-function filterByWord(/*your code here*/){
-  /*your code here*/
+// 2 parameter - array and string
+function filterByWord(array, string){
+   let filteredArray = [];
+   for(let i = 0; i < array.length; i++){
+     if(array[i].includes(string)){
+       filteredArray.push(array[i]);
+     }
+   }
+   return filteredArray;
 }
+
+console.log(filterByWord(originalFlavors, 'Chocolate'));
 
 
 /* 💪💪💪💪💪🧁🍦🍨 STRETCH 🍨🍦🍫💪💪💪💪💪*/ 
@@ -177,7 +210,7 @@ Use the getAverageWordLength function below to do the following:
   For example: getAverageWordLength(originalFlavors) should return a number between 0 and 3.     
 */
 
-function getAverageWordLength(/*code here*/){
+function getAverageWordLength(array){
   /*code here*/
 }
 
